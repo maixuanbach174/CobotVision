7c7e709acebf682d0a37eb298c80c3d5ce23a809
maixuanbach174 <maixuanbach174@gmail.com>
2024-12-03 16:04:17 +0900

    add 4 actions

--- capture_images.py
+++ capture_images.py
@@ -1,4 +1,10 @@
 import cv2
+from pathlib import Path
+
+num = '4'
+my_folder = Path("dataset/action_" + num)
+print(my_folder)
+my_folder.mkdir(exist_ok=True, parents=True)
 
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
@@ -6,7 +12,7 @@ ret, frame = cap.read()
 while(True):
     cv2.imshow('img', frame)
     if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite('dataset/action1/image.png', frame)
+        cv2.imwrite(str(my_folder) + '/image.png', frame)
         cv2.destroyAllWindows()
         break
 cap.release()
