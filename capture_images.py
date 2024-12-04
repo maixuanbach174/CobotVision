d661d0e9c5ce6c19486bc3aeda96a64b4842f59f
maixuanbach174 <maixuanbach174@gmail.com>
2024-12-04 11:20:01 +0900

    add sample data

--- capture_images.py
+++ capture_images.py
@@ -1,18 +1,17 @@
 import cv2
 from pathlib import Path
 
-num = 'report'
-my_folder = Path("dataset/action_" + num)
-print(my_folder)
-my_folder.mkdir(exist_ok=True, parents=True)
+num = 'dataset/action_report/image20.png'
+
 
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
+frame = frame[254:444, 537:727]
 
 while(True):
     cv2.imshow('img', frame)
     if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite(str(my_folder) + '/image.png', frame)
+        cv2.imwrite(num, frame)
         cv2.destroyAllWindows()
         break
 cap.release()
